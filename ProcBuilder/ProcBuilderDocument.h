f9bfb905a6883e766d0944568c81d536e75362d7
Daniel Iparraguirre <danielux@Daniels-MacBook-Pro.local>
2012-10-16 15:00:02 -0700

    Initial Commit

---
+++ ProcBuilder/ProcBuilderDocument.h
@@ -0,0 +1,13 @@
+//
+//  ProcBuilderDocument.h
+//  ProcBuilder
+//
+//  Created by Daniel Iparraguirre on 10/16/12.
+//  Copyright (c) 2012 Daniel Iparraguirre. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ProcBuilderDocument : NSPersistentDocument
+
+@end
---
+++ ProcBuilder/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ProcBuilder
+//
+//  Created by Daniel Iparraguirre on 10/16/12.
+//  Copyright (c) 2012 Daniel Iparraguirre. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
